7cc7a75c6c0859fa635cea4a1ea56a66c457d5ce
Krishnary07 <91017836+Krishnary07@users.noreply.github.com>
2023-04-16 10:42:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import os
-import openai 
+import openai
 import re
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, APIRouter
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from dotenv import load_dotenv
-from openai import Engine, Completion,api_key
+from openai import Engine, Completion, api_key
 from fastapi.middleware.cors import CORSMiddleware
-
-
-
-
 
 
 app = FastAPI()
@@ -39,232 +35,84 @@ app.add_middleware(
 
 load_dotenv()
 
-openai.api_key = os.getenv("OPEN-AI-API-KEY")
-
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 class Conversation(BaseModel):
     messages: list = []
 
+
+class Message(BaseModel):
+    message: str
+
+
 conversation = Conversation()
 
 # Home page
+
+
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 # API endpoint to generate response
+
+router = APIRouter()
+
+
 @app.post("/response")
-async def generate_response(message: str):
-    global conversation
-    conversation.messages.append({"user": message})
+async def generate_response(data: Message):
 
-    patterns=[
-        r".*python\b",
-        r".*golang\b",
-        r".*javascript\b",
-        r".*java\b",
-        r".*c\b",
-        r".*c++\b",
-        r".*c#\b",
-        r".*php\b",
-        r".*ruby\b",
-        r".*swift\b",
-        r".*kotlin\b",
-        r".*rust\b",
-        r".*scala\b",
-        r".*haskell\b",
-        r".*perl\b",
-        r".*lua\b",
-        r".*erlang\b",
-        r".*elixir\b",
-        r".*clojure\b",
-        r".*coffeescript\b",
-        r".*typescript\b",
-        r".*dart\b",
-        r".*f#\b",
-        r".*r\b",
-        r".*visual basic\b",
-        r".*pascal\b",
-        r".*fortran\b",
-        r".*assembly\b",
-        r".*prolog\b",
-        r".*lisp\b",
-        r".*scheme\b",
-        r".*ocaml\b",
-        r".*haxe\b",
-        r".*nim\b",
-        r".*crystal\b",
-        r".*d\b",
-        r".*dlang\b",
-        r".*nimrod\b",
-        r".*cython\b",
-        r".*cython\b",
-        r".*database\b",
-        r".*sql\b", 
-        r".*mysql\b",
-        r".*postgresql\b",
-        r".*sqlite\b",
-        r".*mongodb\b",
-        r".*redis\b",
-        r".*nosql\b",
-        r".*c++\b",
-        r".*code\b",
-        r".*program\b",
-        r".*coding\b",
-        r".*programming\b",
-        r".*language\b",
-        r".*syntax\b",
-        r".*algorithm\b",
-        r".*function\b",
-        r".*method\b",
-        r".*object\b",
-        r".*class\b",
-        r".*variable\b",
-        r".*loop\b",
-        r".*array\b",
-        r".*list\b",
-        r".*dictionary\b",
-        r".*tuple\b",
-        r".*set\b",
-        r".*string\b",
-        r".*integer\b",
-        r".*float\b",
-        r".*boolean\b",
-        r".*if\b",
-        r".*else\b",
-        r".*elif\b",
-        r".*while\b",
-        r".*for\b",
-        r".*try\b",
-        r".*except\b",
-        r".*raise\b",
-        r".*import\b",
-        r".*from\b",
-        r".*as\b",
-        r".*def\b",
-        r".*return\b",
-        r".*print\b",
-        r".*input\b",
-        r".*open\b",
-        r".*write\b",
-        r".*read\b",
-        r".*close\b",
-        r".*append\b",
-        r".*split\b",
-        r".*join\b",
-        r".*sort\b",
-        r".*reverse\b",
-        r".*map\b",
-        r".*filter\b",
-        r".*reduce\b",
-        r".*lambda\b",
-        r".*decorator\b",
-        r".*classmethod\b",
-        r".*staticmethod\b",
-        r".*property\b",
-        r".*super\b",
-        r".*self\b",
-        r".*__init__\b",
-        r".*__str__\b",
-        r".*__repr__\b",
-        r".*__len__\b",
-        r".*__getitem__\b",
-        r".*__setitem__\b",
-        r".*__delitem__\b",
-        r".*__contains__\b",
-        r".*__call__\b",
-        r".*__add__\b",
-        r".*__sub__\b",
-        r".*__mul__\b",
-        r".*__div__\b",
-        r".*__eq__\b",
-        r".*__ne__\b",
-        r".*__lt__\b",
-        r".*__gt__\b",
-        r".*__le__\b",
-        r".*__ge__\b",
-        r".*__enter__\b",
-        r".*__exit__\b",
-        r".*__iter__\b",
-        r".*__next__\b",
-        r".*__bool__\b",
-        r".*__int__\b",
-        r".*__float__\b",
-        r".*__hash__\b",
-        r".*__name__\b",
-        r".*__doc__\b",
-        r".*__module__\b",
-        r".*__class__\b",
-        r".*__slots__\b",
-        r".*__dict__\b",
-        r".*__weakref__\b",
-        r".*__dir__\b",
-        r".*__file__\b",
-        r".*__package__\b",
-        r".*__loader__\b",
-        r".*__spec__\b",
-        r".*__annotations__\b",
-        r".*__builtins__\b",
-        r".*__cached__\b",
-        r".*__debug__\b",
-        r".*__import__\b",
-        r".*__loader__\b",
-        r".*__main__\b",
-        r".*__name__\b",
-        r".*__package__\b",
-        r".*__path__\b",
-        r".*__spec__\b",
-        r".*__all__\b",
-        r".*__author__\b",
-        r".*__builtins__\b",
-        r".*__cached__\b",
-        r".*__credits__\b",
-        r".*__date__\b",
-    ]
+    try:
+        global conversation
+        conversation.messages.append({"user": data.message})
 
-    if not any(re.search(keyword, message, re.IGNORECASE) for keyword in patterns):
-        response_text = "Please ask me a programming-related question."
-        conversation.messages.append({"bot": response_text})
-        return {"response": response_text}
-    
-    else:
-        response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=f"{message}\n\nResponse:",
-        temperature=0.5,
-        max_tokens=1024,
-        n = 1,
-        stop=None,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
+        patterns = [
+            r".*python\b",
+            r".*golang\b",
+            r".*javascript\b",
+            r".*java\b",
+        ]
 
+        if not any(re.search(keyword, data.message, re.IGNORECASE) for keyword in patterns):
+            response_text = "Please ask me a programming-related question."
+            conversation.messages.append({"bot": response_text})
+            return {"response": response_text}
 
-    conversation.messages.append({"bot": response.choices[0].text.strip()})
+        else:
+            response = openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=f"{Message}\n\nResponse:",
+                temperature=0.5,
+                max_tokens=1024,
+                n=1,
+                stop=None,
+                frequency_penalty=0,
+                presence_penalty=0
+            )
+
+        conversation.messages.append({"bot": response.choices[0].text.strip()})
+    except openai.error.RateLimitError as e:
+        return {"response": "I'm sorry, I'm having trouble connecting to the server. Please try again later."}
     return {"response": response.choices[0].text.strip()}
-    
-
-
-
 
 
 # API endpoint to get conversation
+router = APIRouter()
+
+
 @app.get("/conversation")
 async def get_conversation():
     global conversation
     return conversation
 
 # API endpoint to clear conversation
+
+router = APIRouter()
+
+
 @app.delete("/conversation")
 async def clear_conversation():
     global conversation
     conversation = Conversation()
     return {"message": "Conversation cleared."}
-
-
-
-
-
-
-
